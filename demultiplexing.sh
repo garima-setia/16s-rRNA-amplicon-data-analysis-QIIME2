0af7eca9c0d8a3a391c6a602ef9ee6457838f9ef
garima-setia <110136636+garima-setia@users.noreply.github.com>
2022-08-10 12:33:42 -0500

    Update demultiplexing.sh

--- demultiplexing.sh
+++ demultiplexing.sh
@@ -4,14 +4,14 @@
 source activate qiime2-2022.2
 
 #Demultiplex (Sort individuals into samples)
-#For PairedEnd mode
+#For PairedEnd sequences
 qiime tools import 
   --type ‘SampleData[PairedEndSequencesWithQuality]' 
   --input-path rawreads/ 
   --input-format CasavaOneEightSingleLanePerSampleDirFmt 
   --output-path demux-paired.qza
-
-#For SingleEnd mode
+  
+#For SingleEnd sequences
 qiime tools import 
   --type 'SampleData[SequencesWithQuality]' 
   --input-path rawreads/ 
